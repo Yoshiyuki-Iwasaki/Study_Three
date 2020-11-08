0cba540a11fa6f05d07d622e4691b21928164c2d
Yoshiyuki Iwasaki <yoshiyuki_iwasaki@commude.co.jp>
2020-11-09 02:31:51 +0900

    [add] lesson_5 orbitcontrol 追加

--- lesson_5/index.js
+++ lesson_5/index.js
@@ -23,7 +23,14 @@ function init() {
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height);
   camera.position.set(0, 0, +1000);
-  
+
+  // // カメラコントローラーを作成
+  const controls = new THREE.OrbitControls(camera);
+
+  // // 滑らかにカメラコントローラーを制御する
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
+
   // グループを作成
   const group = new THREE.Group();
   scene.add(group);
@@ -32,9 +39,9 @@ function init() {
 
   for (let i = 0; i < 1000; i++) {
     const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = (Math.random() - 0.5) * 2000;
-    mesh.position.y = (Math.random() - 0.5) * 2000;
-    mesh.position.z = (Math.random() - 0.5) * 2000;
+    mesh.position.x = (Math.random() - 0.5) * 1500;
+    mesh.position.y = (Math.random() - 0.5) * 1500;
+    mesh.position.z = (Math.random() - 0.5) * 1500;
     mesh.rotation.x = Math.random() * 2 * Math.PI;
     mesh.rotation.y = Math.random() * 2 * Math.PI;
     mesh.rotation.z = Math.random() * 2 * Math.PI;
@@ -44,16 +51,20 @@ function init() {
   }
 
   // 光源
-  scene.add(new THREE.DirectionalLight(0xff0000, 2)); // 平行光源
-  scene.add(new THREE.AmbientLight(0x00ffff)); // 環境光源
+  scene.add(new THREE.DirectionalLight(0xFFFFFF, 2)); // 平行光源
+  scene.add(new THREE.AmbientLight(0xFFFFFF)); // 環境光源
 
   // 毎フレーム時に実行されるループイベントです
   tick();
 
   function tick() {
+
+    // カメラコントローラーを更新
+    controls.update();
+
     // グループを回す
-    group.rotateY(0.01);
-    group.rotateX(0.01);
+    group.rotateY(0.005);
+    group.rotateX(0.005);
 
     // レンダリング
     renderer.render(scene, camera);
